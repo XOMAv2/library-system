242dd154188db07cca6597692d192f3b16d7ebdf
Nikita Iksaritsa <xomav2@gmail.com>
2021-10-02 23:33:27 +0300

    Added library panel

--- frontend_service/src/service/frontend/views.cljs
+++ frontend_service/src/service/frontend/views.cljs
@@ -83,6 +83,62 @@
    [:div {:on-click #(.stopPropagation %)}
     [:<> forms]]])
 
+(def ^:private sections
+  [[icons/user-circle "My profile" false]
+   [icons/users "Users" false]
+   [icons/book-open "Books" false]
+   [icons/library "Libraries" true]
+   [icons/shopping-bag "Orders" false]
+   [icons/trending-up "Statistics" false]])
+
+(def ^:private schedule
+  ["Пн: 10:00-19:00"
+   "Пн: 10:00-19:00"
+   "Пн: 10:00-19:00"
+   "Пн: 10:00-19:00"
+   "Пн: 10:00-19:00"])
+
+(defn add-button [{:keys [text on-click]}]
+  [:button {:class ["shadow-3xl py-2 px-4 text-lg font-medium rounded-full text-white"
+                    "bg-blue-500 hover:bg-blue-600 focus:outline-none flex items-center"
+                    "focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"]
+            :type "button"
+            :on-click on-click}
+   [icons/plus]
+   [:span.ml-2 text]])
+
+(defn libraries-panel []
+  [:div.h-full.flex.flex-col.relative
+   [:div.px-1.absolute.z-10.bottom-2.right-2
+    [add-button {:text "Add library" :on-click identity}]]
+   [:div.px-1
+    [:input {:type "text" :class [input-style "w-[30rem]"]}]
+    [:div.h-2]]
+   [:div.overflow-y-auto.flex-grow
+    [:ul.space-y-2.p-1
+     (for [i (range 0 100)]
+       ^{:key i}
+       [:li [:a {:class ["w-[30rem] bg-blue-50 rounded-xl space-y-1 py-2 px-3"
+                         "hover:bg-blue-200 focus:ring-2 focus:ring-offset-2"
+                         "focus:ring-blue-500 focus:outline-none block"]
+                 :href "#"}
+             [:div.flex.flex-row.justify-between.items-center
+              [:h1.font-normal.truncate {:class "text-2xl"} "Библиотека имени Ленина"]
+              [:div.flex.flex-row.gap-1
+               [:button {:class ["rounded-xl transform hover:scale-125 focus:outline-none"
+                                 "focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"]}
+                [icons/pencil]]
+               [:button {:class ["rounded-xl transform hover:scale-125 focus:outline-none"
+                                 "focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                                 "hover:text-red-500 focus:outline-none"]}
+                [icons/trash {:class "stroke-current"}]]]]
+             [:p.font-medium.truncate.leading-snug "ул. Бажова, дом 3, корпус 2"]
+             [:ul
+              (for [[index item] (map-indexed #(vector % %2) schedule)]
+                [:li [:p {:class ["text-sm text-gray-500 font-light"
+                                  (when (> index 0) "leading-tight")]}
+                      item]])]]])]]])
+
 (defn main-panel []
   (let [name (rf/subscribe [::subs/name])]
     [:div
