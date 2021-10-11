4971886f89dea209ea5074ef4f386754d73e2ac5
Nikita Iksaritsa <xomav2@gmail.com>
2021-10-11 10:45:04 +0300

    Get all stats schema fix

--- frontend_service/src/service/frontend/api/gateway.cljs
+++ frontend_service/src/service/frontend/api/gateway.cljs
@@ -132,9 +132,7 @@
 #_"stats"
 (def-re ::add-stat-record [stat-record]        (mr :post "/api/stats" stat-record))
 (def-re ::get-stat-record [uid]                (mr :get (str "/api/stats/" uid)))
-(def-re ::get-all-stat-records [service]       (when service
-                                                 (mr :get "/api/stats" nil {:service service})
-                                                 (mr :get "/api/stats")))
+(def-re ::get-all-stat-records [service]       (mr :get "/api/stats" nil {:service service}))
 (def-re ::update-stat-record [uid stat-record] (mr :patch (str "/api/stats/" uid) stat-record))
 (def-re ::delete-stat-record [uid]             (mr :delete (str "/api/stats/" uid)))
 (def-re ::delete-all-stat-records []           (mr :delete "/api/stats"))
--- stats_service/src/service/stats/router.clj
+++ stats_service/src/service/stats/router.clj
@@ -51,7 +51,7 @@
                 
                 :roles #{"admin"}
                 :middleware [authorization-middleware]}
-      ["" {:get {:parameters {:query [:map [:service {:optional true} schemas/non-empty-string]]}
+      ["" {:get {:parameters {:query [:map [:service {:optional true} [:maybe schemas/non-empty-string]]]}
                  :responses {200 {:body [:map [:stats [:sequential schemas/stat-record-out]]]}}
                  :handler handlers/get-all-stat-records}
            :post {:services-uri services-uri
